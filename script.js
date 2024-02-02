242fe258b2a1bdd161347019609a838f3190b68f
Anthony Nishida Mesquita Junior <118089159+satoanthony@users.noreply.github.com>
2024-02-02 16:25:24 -0300

    HTML CSS
    HTML
    added i tags for footer socials
    
    CSS
    added stylization for footer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.getElementById("header").style.fontSize = "30px";
   } else {
     document.getElementById("header").style.fontSize = "90px";
